654df3d8793266a0dbef139cf5e16367acf0e53e
Matthew Fitzsimmons <fitz.matt.d@gmail.com>
2017-03-25 12:15:45 +0000

    updates

--- my_dmenu.sh
+++ my_dmenu.sh
@@ -1,1 +1,1 @@
-dmenu_run -nb "#073642" -nf "#93A1A1" -fn "Fira Mono-9:medium" -h 13 -sb "#859900" -sf "#073642" -p " run:" -h 14
+dmenu_run -nb "#282828" -nf "#ebdbb2" -fn "Fira Code-16:medium" -sb "#b8bb26" -sf "#282828" -p " run:"
---
+++ my_dmenu_pipe.sh
@@ -0,0 +1,1 @@
+dmenu -nb "#282828" -nf "#ebdbb2" -fn "Fira Code-16:medium" -sb "#b8bb26" -sf "#282828" -p " run:"
