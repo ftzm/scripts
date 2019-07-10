a132e3ffb84d810b1a62dd136c14d98176f4f2ab
ftzm <matthew@fitzsimmons.io>
2019-07-10 21:28:47 +0200

    ubuntu fixes

--- my_dmenu.sh
+++ my_dmenu.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+unset XMODIFIERS # This is set in Ubuntu and fucks up dmenu
 dmenu_run -nb "#282828" -nf "#ebdbb2" -fn "Fira Code-${FONT_SIZE:-16}:medium" -sb "#b8bb26" -sf "#282828" -p " run:"
--- toggle_mouse.sh
+++ toggle_mouse.sh
@@ -2,6 +2,7 @@
 
 declare -i ID
 ID=`xinput list | grep -Eio 'touchpad\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
+echo $ID
 declare -i STATE
 STATE=`xinput list-props $ID|grep 'Device Enabled'|awk '{print $4}'`
 if [ $STATE -eq 1 ]
