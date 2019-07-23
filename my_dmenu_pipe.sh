b10cbd8fbc087a83c5ed9cdf6bb01c7f30a7e82b
ftzm <matthew@fitzsimmons.io>
2019-07-23 08:53:49 +0200

    update dmenu pipe for ubuntu fix

--- my_dmenu_pipe.sh
+++ my_dmenu_pipe.sh
@@ -1,1 +1,3 @@
-dmenu -nb "#282828" -nf "#ebdbb2" -fn "Fira Code-${FONT_SIZE:-16}:medium" -sb "#b8bb26" -sf "#282828" -p " run:"
+#!/usr/bin/env bash
+unset XMODIFIERS # This is set in Ubuntu and fucks up dmenu
+dmenu -i -nb "#282828" -nf "#ebdbb2" -fn "Fira Code-${FONT_SIZE:-16}:medium" -sb "#b8bb26" -sf "#282828" -p " run:" <&0
